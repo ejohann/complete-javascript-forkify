ecccef682db781839e2e5d6e7bc83f0725cf7ef1
Johanne <johanne_lewis@hotmail.com>
2019-08-07 14:26:49 +0100

    Try and catch error

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,12 @@ async function getResults(query){
     const apiKey = 'de17a4d06f14b98217ee495612544deb';
     const apiAppID = 'bbf6d6db';
 
-    const results = await axios(`${baseURL}/search?q=${query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
-    const recipes = results.data.hits;
-    console.log(recipes);
+    try{
+        const results = await axios(`${baseURL}/search?q=${query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
+        const recipes = results.data.hits;
+        console.log(recipes);
+    }catch(error){
+        alert(error);
+    }
 }
 getResults('pizza');
