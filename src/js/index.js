76275c032d587d5fd58e9a20d311668abc3b6f86
Johanne <johanne_lewis@hotmail.com>
2019-08-07 14:41:44 +0100

    Remove search function and import search class and call the search function

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,5 @@
-import axios from 'axios'; 
+import Search from './models/Search';
 
-async function getResults(query){
-    const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
-    const apiKey = 'de17a4d06f14b98217ee495612544deb';
-    const apiAppID = 'bbf6d6db';
-
-    try{
-        const results = await axios(`${baseURL}/search?q=${query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
-        const recipes = results.data.hits;
-        console.log(recipes);
-    }catch(error){
-        alert(error);
-    }
-}
-getResults('pizza');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
