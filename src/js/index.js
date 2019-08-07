d348975300a8b85ea20cec0f6e985485c87b79a4
Johanne <johanne_lewis@hotmail.com>
2019-08-07 12:50:04 +0100

    Test code - another way of importing modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Global app controller
 import str from './models/Search';
 
-import {add as a, multiply as m} from './views/searchView';
+//import {add as a, multiply as m} from './views/searchView';
 
-console.log(`Using imported functions add: ${a(9, 6)} and multiply: ${m(9, 6)}`);
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions add: ${searchView.add(9, 6)} and multiply: ${searchView.multiply(9, 6)}`);
