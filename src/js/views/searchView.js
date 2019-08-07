5e24f8b18ad982f119ef3b20c2845c94ffd68573
Johanne <johanne_lewis@hotmail.com>
2019-08-07 19:37:21 +0100

    Fix bugs in code

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,23 +3,25 @@ import { elements } from './base';
 export const getInput = () => elements.searchInput.value;
 
 const renderRecipe = recipe => {
+
     const getID = (uri) => uri.split('#')[1];
 
     const markup = `
         <li>
-            <a class="results__link" href="#${getID(recipe.uri)}">
+            <a class="results__link" href="#${getID(recipe.recipe.uri)}">
                 <figure class="results__fig">
-                    <img src="${recipe.image}" alt="${recipe.label}">
+                    <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.label}</h4>
-                    <p class="results__author">${recipe.source}</p>
+                    <h4 class="results__name">${recipe.recipe.label}</h4>
+                    <p class="results__author">${recipe.recipe.source}</p>
                 </div>
             </a>
         </li>
     `;
-    elements.searchResultList.insertAdjacentElement('beforeend', markup);
-}
+    
+    elements.searchResultList.insertAdjacentHTML('beforeend', markup);
+};
 
 export const renderResults = recipes => {
     recipes.forEach(renderRecipe);
