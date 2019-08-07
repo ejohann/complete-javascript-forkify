cb3c92bcd1852e5a9c881e8ee7a7007f73690f61
Johanne <johanne_lewis@hotmail.com>
2019-08-07 18:03:24 +0100

    Logic for search controller, with state object and event listener for search button

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,37 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+/**  
+ * Global state of the app
+ * 1. Search object
+ * 2. Current recipe object
+ * 3. Shopping list object
+ * 4. Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query from view
+
+    const query = 'pizza'
+    if(query){
+        // 2. New search object and add to state
+        state.search = new Search(query);
+
+        // 3. prepare UI for results
+
+        // 4 Search for recipes
+        await state.search.getResults();
+
+        // 5. render results for recipes
+        console.log(state.search.recipes); 
+    }
+  };
+
+  // event listener on search form
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+  });
+
+
