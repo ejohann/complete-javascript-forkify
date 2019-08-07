9f726a43e64bfeb7a27e86ce7aefe8631afa4e37
Johanne <johanne_lewis@hotmail.com>
2019-08-07 14:41:05 +0100

    Create a search class with a search method - log results to console for testing

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+import axios from 'axios'; 
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
+        const apiKey = 'de17a4d06f14b98217ee495612544deb';
+        const apiAppID = 'bbf6d6db';
+    
+        try{
+            const results = await axios(`${baseURL}/search?q=${this.query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
+            this.recipes = results.data.hits;
+            console.log(this.recipes);
+        }catch(error){
+            alert(error);
+        }
+    }
+}
