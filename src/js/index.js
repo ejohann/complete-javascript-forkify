f031283afbba4c4208d31796da0464f171cdffae
Johanne <johanne_lewis@hotmail.com>
2019-08-07 12:47:46 +0100

    Test code - Import modules from searchView and log to console

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,6 @@
 // Global app controller
-Import str from './models/Search';
+import str from './models/Search';
+
+import {add as a, multiply as m} from './views/searchView';
+
+console.log(`Using imported functions add: ${a(9, 6)} and multiply: ${m(9, 6)}`);
