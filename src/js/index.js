5a69cac6d5b7c812b2da42a5ddc7884f1db6e935
Johanne <johanne_lewis@hotmail.com>
2019-08-07 14:16:24 +0100

    Create function to make call to recipe api

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import axios from 'axios'; 
 
+async function getResults(query){
+    const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
+    const apiKey = 'de17a4d06f14b98217ee495612544deb';
+    const apiAppID = 'bbf6d6db';
+
+    const results = await axios(`${baseURL}/search?q=${query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
+    console.log(results);
+}
+getResults();
