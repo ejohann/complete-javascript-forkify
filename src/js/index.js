d301c463cdeed537330407de830e26e6a3286470
Johanne <johanne_lewis@hotmail.com>
2019-08-07 12:53:38 +0100

    Delete test code

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
 // Global app controller
-import str from './models/Search';
-
-//import {add as a, multiply as m} from './views/searchView';
-
-import * as searchView from './views/searchView';
-
-console.log(`Using imported functions add: ${searchView.add(9, 6)} and multiply: ${searchView.multiply(9, 6)}`);
