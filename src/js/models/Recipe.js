60fa5952fbd048159651bbfa4507488cd9bcf1d1
Johanne <johanne_lewis@hotmail.com>
2019-08-08 15:01:16 +0100

    Add properties to object from recipe results

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,7 +10,13 @@ export default class Recipe{
         try{
             const recipeID = encodeURIComponent(`${recipeURL}${this.id}`)
             const results = await axios(`${baseURL}/search?r=${recipeID}&app_id=${apiAppID}&app_key=${apiKey}`);
-            console.log(results);
+            this.title = results.data[0].label;
+            this.author = results.data[0].source;
+            this.image = results.data[0].image;
+            //this.url = results.data[0].url;
+            this.url = results.data[0].shareAs;
+            this.ingredients = results.data[0].ingredientLines;
+           console.log(results);
         }catch(error){
             console.log(error);
         }
