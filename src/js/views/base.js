41861b12d25089308a79a8748d960c9be2fc97f1
Johanne <johanne_lewis@hotmail.com>
2019-08-08 09:15:59 +0100

    Create function for displaying a spinner on loading, add element to DOM objects

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,17 @@
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchResult: document.querySelector('.results'),
     searchResultList: document.querySelector('.results__list')
+  };
+
+  export const renderLoader = (parent) => {
+    const loader = `
+        <div class="loader">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg
+        </div>
+        `;
+        parent.insertAdjacentHTML('afterbegin', loader);
   };
