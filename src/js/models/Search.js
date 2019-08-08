fbcc059dfc71057fc25b8c9d7a3e3b0c96ed0940
Johanne <johanne_lewis@hotmail.com>
2019-08-08 14:17:23 +0100

    Import variables from configuration file

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,15 +1,12 @@
 import axios from 'axios'; 
+import { baseURL, apiAppID, apiKey } from '../config';
 
 export default class Search{
     constructor(query){
         this.query = query;
     }
 
-    async getResults(){
-        const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
-        const apiKey = 'de17a4d06f14b98217ee495612544deb';
-        const apiAppID = 'bbf6d6db';
-    
+    async getResults(){  
         try{
             const results = await axios(`${baseURL}/search?q=${this.query}&from=0&to=50&app_id=${apiAppID}&app_key=${apiKey}`);
             this.recipes = results.data.hits;
