6f81196b9f9472b9496c83c1ef778410752ce441
Johanne <johanne_lewis@hotmail.com>
2019-08-08 14:16:29 +0100

    Fix bug in code

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
-const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
-const apiKey = 'de17a4d06f14b98217ee495612544deb';
-const apiAppID = 'bbf6d6db';
+export const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
+export const apiKey = 'de17a4d06f14b98217ee495612544deb';
+export const apiAppID = 'bbf6d6db';
