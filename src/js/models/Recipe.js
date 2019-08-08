fe30d13c0b013f2f14d2007eb39c032e3caa7056
Johanne <johanne_lewis@hotmail.com>
2019-08-08 16:33:26 +0100

    Function with structure for parsing ingredients array

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,4 +33,16 @@ export default class Recipe{
     calcServings(){
         this.servings = 4;
     }
+
+    parseIngredients (){
+        const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+
+            // 2.  Remove parenthesis
+
+            // 3. parse ingredients into count, unit and ingredient
+        });
+        this.ingredients = newIngredients;
+    }
+
 }
