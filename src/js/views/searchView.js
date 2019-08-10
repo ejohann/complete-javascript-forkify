a7dfaff9fc94a2c8727fed9bb19c094c2b77cbab
Johanne <johanne_lewis@hotmail.com>
2019-08-10 14:09:04 +0100

    Fix bug in code

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -18,7 +18,7 @@ export const highLightSelected = id => {
             el.classList.remove('results__link--active');
       });
 
-    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
   };
 
 const limitRecipeTitle = (recipeTitle, limit = 17) => {
