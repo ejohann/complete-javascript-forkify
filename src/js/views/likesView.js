f270d49753cc84eadbf09266594b1be70797bfd3
Johanne <johanne_lewis@hotmail.com>
2019-08-10 14:00:42 +0100

    Function for adding a like item in like menu

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -8,3 +8,20 @@ export const toggleLikeBtn = isLiked => {
 export const toggleLikeMenu = totalLikes => {
     elements.likesMenu.style.visibility = totalLikes > 0 ? 'visible' : 'hidden';
 };
+
+export const renderLike = like => {
+    const markup = `
+        <li>
+            <a class="likes__link" href="#${like.id}">
+                <figure class="likes__fig">
+                    <img src="${like.image}" alt="${like.title}">
+                </figure>
+                <div class="likes__data">
+                    <h4 class="likes__name">${like.title}</h4>
+                    <p class="likes__author">${like.author}</p>
+                </div>
+            </a>
+        </li>
+    `;
+    elements.likesList.insertAdjacentHTML('beforeend', markup);
+};
