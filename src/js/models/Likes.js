7744cacac2508947dda8f4b27a12133f0b7abce0
Johanne <johanne_lewis@hotmail.com>
2019-08-10 12:10:14 +0100

    Method for adding a like

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,6 +1,18 @@
 export default class Likes{
     constructor(){
         this.likes = [];
-    }
+      }
+    
+    addLike(id, title, author, image){
+        const like = {
+            id,
+            title,
+            author,
+            image
+          };
+        
+          this.likes.push(like);
+          return like;
+      }
 
 }
