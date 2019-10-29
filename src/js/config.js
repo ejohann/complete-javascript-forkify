285456f64d8ce20d7a31c518a425392178679477
Johanne <johanne_lewis@hotmail.com>
2019-10-29 07:04:42 +0000

    Change http to https for production version

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://cors-anywhere.herokuapp.com/https://api.edamam.com';
+export const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.edamam.com';
 export const apiKey = 'de17a4d06f14b98217ee495612544deb';
 export const apiAppID = 'bbf6d6db';
 export const recipeURL = 'http://www.edamam.com/ontologies/edamam.owl#';
